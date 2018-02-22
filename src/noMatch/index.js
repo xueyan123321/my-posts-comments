f8edf71c1741feb2d8ff0659e3c50998b521e885
xueyan123321 <1152991535@qq.com>
2018-02-22 11:19:15 +0800

    feat:add the navigation button in 404 page

--- src/noMatch/index.js
+++ src/noMatch/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import './noMatch.css'
+import { Button, Icon } from 'antd'
+import { Link } from 'react-router-dom'
 
 const NoMatch = () => (
     <div className="not-found">
         <strong>
             not found 404
         </strong>
+        <div className="back-to-main">
+            <Link to={'/'}>
+                <Button type="primary">
+                    <Icon type="left" />
+                    back to post list
+                </Button>
+            </Link>
+        </div>
     </div>
 )
 
